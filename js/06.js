733bfdc2f3cec994e01ea0855cc447d9870dfb99
Andrea Sierra <andrea18.hs@gmail.com>
2025-05-26 15:11:25 -0600

    Math en JS

--- js/06.js
+++ js/06.js
@@ -1,7 +1,31 @@
 // Objeto Math
-let result;
+let result1;
+let result2;
+let result3;
+let result4;
+let result5;
+let result6;
+let result7;
+let result8;
+let result9;
 
-result = Math.PI;
-result = Math.round(2.2);
+result1 = Math.PI;
+result2 = Math.round(2.4); // Round redondea hacia arriba a partir del 0.5
+result3 = Math.ceil(2.1); // Ceil redondea hacia arriba
+result4 = Math.floor(2.9); // Floor redondea hacia abajo
+result5 = Math.sqrt(144); 
+result6 = Math.abs(-200); 
+result7 = Math.min(2, 5, 7, 15, -1); 
+result8 = Math.max(2, 5, 7, 15, -1); 
+result9 = Math.floor( Math.random() * 30 );
 
-console.log(result);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+console.log(result7);
+console.log(result8);
+console.log(result9);
