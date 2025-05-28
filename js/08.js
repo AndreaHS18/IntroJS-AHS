a8cd67e96c502bea7a9c2cfc078a70572386c06b
Andrea Sierra <andrea18.hs@gmail.com>
2025-05-28 12:47:19 -0600

    Concatenación y Template strings en JS

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const name = 'Andrea';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log("Nombre cliente: " + name + "\nCorreo cliente: " + email);
+console.log("Nombre cliente: " , name , "\nCorreo cliente " , email);
+
+// Template Strings - String Literals
+console.log(`Nombre cliente: ${name} \nCorreo cliente: ${email}`);
