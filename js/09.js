7da838b79cb3f42f2eb72c133b397048556cff51
Andrea Sierra <andrea18.hs@gmail.com>
2025-05-28 13:39:03 -0600

    Booleans en JS

---
+++ js/09.js
@@ -0,0 +1,7 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = new Boolean(true);
+
+console.log(typeof boolean1, boolean1);
+console.log(typeof boolean2, boolean2);
+console.log(typeof boolean3, boolean3);
